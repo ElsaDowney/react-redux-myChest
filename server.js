a2d2da981b562c458a92062e0413792fbce967da
xiaopanglzhu <1522898103@qq.com>
2016-08-23 21:21:04 +0800

    finish page refresh

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const regRouters = require('./server/routers/index');
+const path = require('path');
 
 const app = new express();
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('./public'));
-
-
 regRouters(app);
 
+app.get('*', function (request, response){
+  response.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 var server = app.listen(3000, function () {
   console.log('listening at port %s', server.address().port);
