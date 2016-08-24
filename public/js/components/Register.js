10f6537eb0b3deeb85867fcd6bad3478e9c2d2f4
mahong <1025383143@qq.com>
2016-08-24 14:10:42 +0800

    add style

--- public/js/components/Register.js
+++ public/js/components/Register.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+// import {browserHistory} from 'react-router';
 
 class Register extends Component {
 
@@ -24,58 +25,63 @@ class Register extends Component {
   judgeUserName() {
     const username = this.refs.myName.value;
     if (username.length < 2) {
-      $('.user').append('<div class="remarkName">' + "长度应不小于2位!请修改!" + '</div>');
+      $("#p1").show();
     } else {
-      $(".remarkName").remove();
+      $("#p1").hide();
     }
   }
 
-  judgePassword() {
-    const password = this.refs.myPassword.value;
-    if (password.length < 6 || password.length > 12) {
-      $('.password').append('<div class="remarkPassword">' + "密码应为6到12位!请修改!" + '</div>');
-    } else {
-      $(".remarkPassword").remove();
-    }
+judgePassword(){
+  const password = this.refs.myPassword.value;
+  if (password.length < 6 || password.length > 12) {
+    $("#p2").show();
+  } else {
+    $("#p2").hide();
+  }
+}
+
+
+render()
+{
+  if (this.props.registerState === 'success') {
+    alert('注册成功');
+  }
+  if (this.props.registerState === 'fail') {
+    alert('用户已存在');
   }
 
+  return <div>
+    <div className="user">
+      <input type="text" className="form-control" ref="myName"
+             onChange={this.judgeUserName.bind(this)}
+             id="user" placeholder="请输入用户名"/>
 
-  render() {
+      <p id="p1">亲,用户名长度应不小于2位!</p>
 
-    if (this.props.registerState==='success') {
-      alert('注册成功');
-    }
-    if (this.props.registerState==='fail') {
-      alert('用户已存在');
-    }
-
-    return <div>
-      <div className="user">
-        <input type="text" className="form-control" ref="myName"
-               onChange={this.judgeUserName.bind(this)}
-               id="user" placeholder="请输入用户名"/>
-      </div>
-      <br/>
-      <div className="password form-group">
-        <input type="password" className="form-control" ref="myPassword"
-               onClick={this.judgePassword.bind(this)}
-               id="password" placeholder="请输入密码"/>
-      </div>
-      <br/>
-      <div className="repeatPassword form-group">
-        <input type="password" className="form-control" ref="repeatPassword"
-               id="repeatPassword"
-               placeholder="请再次确认密码"/>
-      </div>
-      <hr/>
-      <div className="buttonCenter">
-        <button type="submit"
-                onClick={this.commit.bind(this)}
-                className="btn btn-default">注册
-        </button>
-      </div>
     </div>
-  }
+    <br/>
+    <div className="password form-group">
+      <input type="password" className="form-control" ref="myPassword"
+             onClick={this.judgePassword.bind(this)}
+             id="password" placeholder="请输入密码"/>
+      <p id="p2">亲,密码应为6到12位!</p>
+
+    </div>
+    <br/>
+    <div className="repeatPassword form-group">
+      <input type="password" className="form-control" ref="repeatPassword"
+             id="repeatPassword"
+             placeholder="请再次确认密码"/>
+    </div>
+    <hr/>
+    <div className="buttonCenter">
+      <button type="submit"
+              onClick={this.commit.bind(this)}
+              className="btn btn-default">注册
+      </button>
+    </div>
+  </div>
+}
 }
 
 
