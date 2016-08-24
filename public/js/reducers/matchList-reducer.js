b1569734d56e1ef44c23c3d4213fe56ad87e484c
yhl221 <1727538058@qq.com>
2016-08-24 10:29:32 +0800

    build matchList reducer

---
+++ public/js/reducers/matchList-reducer.js
@@ -0,0 +1,12 @@
+
+function matchList (state = {user: []},action) {
+  switch (action.type) {
+    case 'GETMATCH':
+      return {user:action.user.clo_list};
+  }
+  return state;
+}
+module.exports=matchList;
+
+
+
