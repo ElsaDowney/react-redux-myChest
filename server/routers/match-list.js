5fd49b8853ed03669b0a628df9e7460b4115c3de
yhl221 <1727538058@qq.com>
2016-08-24 10:35:07 +0800

    build server

---
+++ server/routers/match-list.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+
+const AllMatches = require('../dbs/match-list');
+
+router.get('/match', (req, res)=> {
+  const userName='xiaopangzhu';
+ AllMatches(userName,function(result){
+    res.json(result).end();
+  });
+});
+
+module.exports = router;
+
